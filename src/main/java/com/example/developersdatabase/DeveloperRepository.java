0a40c6ed723a0e86c6e38ff9eb132650a75de859
Robin Rawat <robin.rawat@hashstudioz.com>
2022-05-24 21:36:23 +0530

    Initial commit

---
+++ src/main/java/com/example/developersdatabase/DeveloperRepository.java
@@ -0,0 +1,7 @@
+package com.example.developersdatabase;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DeveloperRepository extends JpaRepository<Developer, Long> {
+
+}
